90de0679ac997370c5a8f08890c22f47588fa40c
bql20000 <bql20000@gmail.com>
2021-05-15 21:27:53 +0700

    fix: Prevent running tests in dev environment

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from main.extensions import db
@@ -5,6 +7,10 @@ from main.app import app
 from tests.helpers import create_db_samples
 
 
+env = os.getenv("ENVIRONMENT")
+if env != 'test':
+    print('Tests should be run in test environment')
+    exit(1)
 
 
 def reset_db():
